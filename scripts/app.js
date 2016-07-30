b2aa46fe40b209babdf0cea62f2529a6faffc581
tbduong <tiffaniduong@gmail.com>
2016-07-30 16:07:28 -0700

    Success in connecting model,controller, to view

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,40 @@
 console.log("POKEDEX online");
 angular
-.module('Pokedex, []')
-.controller('PokemonIndexCtrl', PokemonIndexCtrl);
+.module('pokedex',[])
+.controller('PokemonIndexController', PokemonIndexController);
 
-PokemonIndexCtrl.$inject = ['$http']; //inject use of $http to hit endpoints
+PokemonIndexController.$inject = [ '$http' ]; //inject use of $http to hit endpoints
+function PokemonIndexController( $http ){
+  var pm = this;
 
-function PokemonIndexCtrl( $http){
+//GET all PokemonIndexCtrl
+$http({
+    method: 'GET',
+    url: 'https://super-crud.herokuapp.com/pokemon'
+  }).then(function successCallback(response) {
+    pm.pokemonList = response.data.pokemon;
+    console.log("SUCCESS found:", pm.pokemonList);
+  }, function errorCallback(response) {
+      console.log("ERROR! Error retrieving data,", response);
+});
 
-var pk = this;
-pk.newPokemon = {};
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
